2e291436bc48294c5c5fe80d1af87429c8df8fe1
Tigelt <Tigelt2014@gmail.com>
2025-10-05 21:08:33 +0700

    Update: 2025-10-05 21:08:33

--- core/handlers/order_handler.py
+++ core/handlers/order_handler.py
@@ -292,7 +292,7 @@ class OrderHandler:
             transfer = ''
             
             payment = order_data.get('payment_method', '').lower()
-            summa = order_data.get('total', 0)
+            summa = order_data.get('main_sum', 0)
             
             
             
